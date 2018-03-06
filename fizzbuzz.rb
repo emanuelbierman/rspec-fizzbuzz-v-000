4e72cfc6ff24fc694371db2af791b02a125ff675
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-03-06 17:25:39 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,9 @@ def fizzbuzz(num)
   num = num.to_i
   if num % 5 == 0
   return "Buzz"
-elsif condition
-   num % 3 == 0
+  elsif num % 3 == 0
   return "Fizz"
-elsif condition
-   num % 3 == num % 5
+  elsif num % 3 == num % 5
   return "FizzBuzz"
   else
   return nil
